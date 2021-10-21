838e851562def215431b5cd4db375c6499332524
klamaj <kri.lamaj@gmail.com>
2021-10-21 17:40:49 +0300

    Site menu creation

--- wp-content/themes/oliorama/templates/header/site-menu.php
+++ wp-content/themes/oliorama/templates/header/site-menu.php
@@ -1,7 +1,20 @@
 <section id="nav" class="fixed w-full h-full top-0 left-0 bg-danger pl-12 pr-9 pt-10 pb-16">
     <div>
         <nav class="grid">
-
+            <!-- Title -->
+            <div class="pb-6" type="text">
+                <h1 class="text-gray1 font-normal text-2xl">Products</h1>
+            </div>
+            <!-- Line -->
+            <div class="border bg-secondary border-solid"></div>
+            <!-- Menus -->
+            <div>
+                <ul class="list-none float-left">
+                    <li class="">1</li>
+                    <li class="">1</li>
+                    <li class="">1</li>
+                </ul>
+            </div>
         </nav>
     </div>
 </section>
