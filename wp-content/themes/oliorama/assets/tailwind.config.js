ae7de0c5f2c71849376820d94d8fadcc4058c2f0
klamaj <kri.lamaj@gmail.com>
2021-10-21 16:22:21 +0300

    Mobile Header Completed

--- wp-content/themes/oliorama/assets/tailwind.config.js
+++ wp-content/themes/oliorama/assets/tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // background color
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#0C0C0C',
+        'secondary': '#4C4C4C',
+        'danger': '#e3342f',
+      }),
+
+      // text color
+      textColor: theme => theme('colors'),
+      textColor: {
+        'gray1': '#B2B2B2',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      }
+    },
   },
   variants: {
     extend: {},
