cca525b7c6c52f2dadff693e9dccebffb20d6faf
klamaj <kri.lamaj@gmail.com>
2021-11-10 17:14:27 +0200

    def sidebar template

--- wp-content/themes/oliorama/page.php
+++ wp-content/themes/oliorama/page.php
@@ -1,14 +1,18 @@
 <!-- Header -->
 <?php get_header(); ?>
 
-<!-- Page Content -->
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<div class="bg-davys-gray">
 
-    <?php the_content()?>
+    <!-- Page Content -->
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-<?php endwhile; else : endif; ?>
+        <?php the_content()?>
 
-<?php if(is_page('Shop'))  get_template_part( 'templates/pages/test_page' ); ?>
+    <?php endwhile; else : endif; ?>
+
+    <?php if(is_page('Shop'))  get_template_part( 'templates/pages/test_page' ); ?>
+    
+</div>
     
 <!-- Footer -->
 <?php get_footer(); ?>
