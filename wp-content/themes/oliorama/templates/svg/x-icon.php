e7d7d31f29a9e09040d77ff3701414f664aa5cc6
klamaj <kri.lamaj@gmail.com>
2022-01-04 21:21:24 +0200

    set icons templates | x-icon

---
+++ wp-content/themes/oliorama/templates/svg/x-icon.php
@@ -0,0 +1,6 @@
+<svg class="x-icon" xmlns="http://www.w3.org/2000/svg" width="26.121" height="26.121" viewBox="0 0 26.121 26.121">
+    <g transform="translate(-1126.182 -675.182)">
+        <line class="a" x2="24" y2="24" transform="translate(1127.243 676.243)"/>
+        <line class="a" x1="24" y2="24" transform="translate(1127.243 676.243)"/>
+    </g>
+</svg>
